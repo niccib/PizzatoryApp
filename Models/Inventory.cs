beedd5a765de5c8aa073eaf8ac529dac29063814
Nicole Bishop <nicolejayne915@hotmail.com>
2020-03-26 12:13:18 -0500

    Added Error messages and data annotations

--- Models/Inventory.cs
+++ Models/Inventory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,19 @@ namespace PizzatoryApp.Models
 {
     public class Inventory
     {
+        [Required(ErrorMessage = "You must enter a product name")]
+        [StringLength(100, MinimumLength = 3)]
+        [Display(Name = "Product Name")]
         public string ItemName { get; set; }
+
+        [Range(1, 9999, ErrorMessage = "enter amount under 10000")]
+        [Required(ErrorMessage = "You must enter an amount needed")]
+        [Display(Name = "Stock Needed")]
         public int StockNeeded { get; set; }
+
+        [Range(0, 9999)]
+        [Required(ErrorMessage = "You must enter the amount in stock")]
+        [Display(Name = "Amount Currently On Hand")]
         public int OnHand { get; set; }
 
     }
