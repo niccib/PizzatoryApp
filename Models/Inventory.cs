4a55a1021e803825c5f7ad3f09ef46ba9eb9611d
Nicole Bishop <nicolejayne915@hotmail.com>
2020-03-26 11:50:10 -0500

    **fingers crossed** hope this works

---
+++ Models/Inventory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzatoryApp.Models
+{
+    public class Inventory
+    {
+        public string ItemName { get; set; }
+        public int StockNeeded { get; set; }
+        public int OnHand { get; set; }
+
+    }
+}
